8712f63e20f08f04cb3fa057d34cf9aaa3c872be
zgyj4532 <138000358+zgyj4532@users.noreply.github.com>
2024-09-15 12:41:03 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+import shutil
+from pathlib import Path
+
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as OB11Adapter
+
+def clean_pycache(path: Path = Path()) -> None:
+    for p in path.iterdir():
+        if not p.is_dir():
+            continue
+        if p.name == "__pycache__":
+            shutil.rmtree(p)
+        else:
+            clean_pycache(p)
+
+
+nonebot.init()
+driver = nonebot.get_driver()
+driver.register_adapter(OB11Adapter)
+
+nonebot.load_plugin("nonebot_plugin_werewolf")
+
+if __name__ == "__main__":
+    try:
+        nonebot.run()
+    finally:
+        clean_pycache()
