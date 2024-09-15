f8eba33e6789f0bfcec0b6a1976199b5d58429c3
zgyj4532 <138000358+zgyj4532@users.noreply.github.com>
2024-09-15 12:42:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter(OB11Adapter)
 
-nonebot.load_plugin("nonebot_plugin_werewolf")
+nonebot.load_plugin("nonebot_plugin_cardgame_niuniu_master")
 
 if __name__ == "__main__":
     try:
